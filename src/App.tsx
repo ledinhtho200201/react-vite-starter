9aebfc561d90b1d00d27b288e4735b47ee844dbb
thold <thole@gmail.com>
2024-07-03 07:24:09 +0700

    useDispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from './redux/store'
+import { increment } from './redux/counter/counter.slice'
 
 function App() {
   const count = useSelector((state: RootState) => state.counter)
-  console.log(">>> check count: ", count)
+
+  const dispatch = useDispatch();
 
   return (
     <>
@@ -21,7 +23,7 @@ function App() {
       <div>
         <h1>My current count = {count.value}</h1>
         <div>
-          <button>Increase + 1</button>
+          <button onClick={() => dispatch(increment())}>Increase + 1</button>
         </div>
       </div>
     </>
